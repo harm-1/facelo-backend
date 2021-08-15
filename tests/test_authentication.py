47b6bc63be8874e34d66602d88fec6356806b1f0
Harm van Eekeren <harmvaneekeren@gmail.com>
2021-08-15 13:46:17 +0200

    added bad token test, and parameters for login response code

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -9,14 +9,14 @@ from facelo.exceptions import USER_ALREADY_REGISTERED, USER_NOT_FOUND, USER_PASS
 from .factories import UserFactory
 
 @pytest.fixture
-def user_dict():
-    user_dict = UserFactory.stub().__dict__
+def user_dict(kwargs):
+    user_dict = UserFactory.stub(**kwargs).__dict__
     user_dict['birth_day'] = str(user_dict['birth_day'])
     return user_dict
 
 @pytest.fixture
-def register_user(client, user_dict, kwargs):
-    return client.post(url_for("user.register_user"), json=user_dict, **kwargs)
+def register_user(client, user_dict):
+    return client.post(url_for("user.register_user"), json=user_dict)
 
 
 @pytest.mark.usefixtures('db')
@@ -27,30 +27,25 @@ class TestAuthenticate:
         assert register_user.json['token'] != 'None'
         assert register_user.json['token'] != ''
 
+    def test_register_already_registered_user(self, client, user_dict, register_user):
+        resp = client.post(url_for("user.register_user"), json=user_dict)
+        assert resp.status_code == 422
+        assert resp.json == USER_ALREADY_REGISTERED['message']
+
     def test_user_login(self, client, user_dict, register_user):
         resp = client.post(url_for('user.login_user'), json=user_dict)
-
         assert resp.json['email'] == user_dict['email']
         assert resp.json['token'] != 'None'
         assert resp.json['token'] != ''
 
-    def test_user_login_wrong_email(self, client, register_user):
-        resp = client.post(url_for('user.login_user'), json={
-            'email': 'incorrect@stuff.com',
-            'password': 'incorrect'
-        })
-
-        assert resp.status_code == 404
-        assert resp.json == USER_NOT_FOUND['message']
-
-    def test_user_login_wrong_password(self, client, user_dict, register_user):
-        resp = client.post(url_for('user.login_user'), json={
-            'email': user_dict['email'],
-            'password': 'incorrect'
-        })
-
-        assert resp.status_code == 401
-        assert resp.json == USER_PASSWORD_INCORRECT['message']
+    @pytest.mark.kwargs(email='foo@bar.com', password='foobar')
+    @pytest.mark.parametrize("credentials,status_code", [
+        ({'email': 'foo@bar.com', 'password': 'foobar'}, 200),
+        ({'email': 'bar@foo.com', 'password': 'foobar'}, 404),
+        ({'email': 'foo@bar.com', 'password': 'barfoo'}, 401),])
+    def test_login_response_codes(self, client, register_user, credentials, status_code):
+        resp = client.post(url_for('user.login_user'), json=credentials)
+        assert resp.status_code == status_code
 
     def test_get_user(self, client, user_dict, register_user):
         token = str(register_user.json['token'])
@@ -59,15 +54,15 @@ class TestAuthenticate:
         })
         assert resp.json['email'] == user_dict['email']
 
-    def test_register_already_registered_user(self, client, user_dict, register_user):
-        resp = client.post(url_for("user.register_user"), json=user_dict)
-
-        assert resp.status_code == 422
-        assert resp.json == USER_ALREADY_REGISTERED['message']
-
     def test_update_user(self, client, user_dict, register_user):
         token = str(register_user.json['token'])
         resp = client.put(url_for('user.update_user'), json=user_dict, headers={
             'Authorization': 'Bearer {}'.format(token)
         })
         assert resp.json['email'] == user_dict['email']
+
+    def test_bad_authorization(self, client, user_dict, register_user):
+        resp = client.get(url_for('user.get_user'), headers={
+            'Authorization': ''
+        })
+        assert resp.status_code == 401
