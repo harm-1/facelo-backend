f4f9d37d6ab33f03e77e371a89e0ca9d43165573
Harm van Eekeren <harmvaneekeren@gmail.com>
2021-08-30 22:20:50 +0200

    image models

---
+++ facelo/image/models.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""Image models."""
+import datetime as dt
+
+from facelo.database import Column, Model, SurrogatePK, db
+
+from sqlalchemy.dialects.mysql import TINYINT
+
+class Image(SurrogatePK, Model):
+
+    __tablename__ = 'images'
+    image_url = Column(db.String(100), nullable=False)
+    created_date = Column(db.DateTime, nullable=False)
+    upload_date = Column(db.DateTime, nullable=False)
+    age_in_foto = Column(TINYINT(unsigned=True))
+
+    def __repr__(self):
+        """Represent instance as a unique string."""
+        return '<User({image_url!r})>'.format(image_url=self.image_url)
