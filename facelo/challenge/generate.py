c5ef4f7985ab727a0bdcc77e53c5082c593e21fd
harm-1 <hveekeren.d@gmail.com>
2021-11-08 18:07:03 +0100

    Replacing int with trial, checking for use.

--- facelo/challenge/generate.py
+++ facelo/challenge/generate.py
@@ -10,16 +10,15 @@ from facelo.challenge.models import Challenge
 from facelo.constants import (CHALLENGE_TYPE_RANDOM, CHALLENGE_TYPE_SAMETRIAL,
                               CHALLENGE_TYPE_TRIANGLE)
 from facelo.trial.models import Trial
+from facelo.user.models import User
 from sqlalchemy.sql.functions import random as sql_random
 
 
-# TODO should be checked for lower than a number of hidden votes, but will refactor also probably
 def random_trial_generator():
     if Trial.query.count() == 0:
         return None
     while True:
-        trials = Trial.query.order_by(sql_random()).limit(2).all()
-        # breakpoint()
+        trials = Trial.query.order_by(sql_random()).limit(100).all()
         for trial in trials:
             yield trial
 
@@ -27,57 +26,48 @@ def random_trial_generator():
 random_trials = random_trial_generator()
 
 
-# TODO filter for trials of the user itself
-# I think I only dont want to send the same ones in the newly created with respect to the last 50.
 def generate_challenges(to_create: dict[int, int], uncompleted: list[Challenge],
-                        completed: list[Challenge]) -> list[dict[str, int]]:
+                        completed: list[Challenge], user: User) -> list[dict[str, int]]:
     """ This function generates data which can be used to create new challenges. Its main job is choose trials.
     The trials ca"""
     not_in_filter = created_challenges_list(uncompleted + completed)
-    # generated_triangle = gen_triangle(to_create[CHALLENGE_TYPE_TRIANGLE], completed, not_in_filter)
+    generated_triangle = gen_triangle(to_create[CHALLENGE_TYPE_TRIANGLE], completed, user,
+                                      not_in_filter)
 
-    # not_in_filter.extend(generated_triangle)
-    generated_sametrial = gen_sametrial(to_create[CHALLENGE_TYPE_SAMETRIAL], completed,
+    not_in_filter.extend(generated_triangle)
+    generated_sametrial = gen_sametrial(to_create[CHALLENGE_TYPE_SAMETRIAL], completed, user,
                                         not_in_filter)
 
     not_in_filter.extend(generated_sametrial)
-    generated_random = gen_random(to_create[CHALLENGE_TYPE_RANDOM], not_in_filter)
+    generated_random = gen_random(to_create[CHALLENGE_TYPE_RANDOM], user, not_in_filter)
 
     generated: list[dict[str, int]] = []
     for data in generated_random:
         generated.append({'trial_1': data[0], 'trial_2': data[1], 'type': CHALLENGE_TYPE_RANDOM})
     for data in generated_sametrial:
         generated.append({'trial_1': data[0], 'trial_2': data[1], 'type': CHALLENGE_TYPE_SAMETRIAL})
-    # for data in generated_triangle:
-    # generated.append({'trial_1': data[0], 'trial_2': data[1], 'type': CHALLENGE_TYPE_TRIANGLE})
+    for data in generated_triangle:
+        generated.append({'trial_1': data[0], 'trial_2': data[1], 'type': CHALLENGE_TYPE_TRIANGLE})
 
     shuffle(generated)
     return generated
 
 
-# TODO bugged, sort() returns none, trials can be none
-def created_challenges_list(challenges: list[Challenge]) -> list[list[int]]:
-
-    result = []
-    for challenge in challenges:
-        trials = [challenge.winner_id, challenge.loser_id]
-        trials.sort(key=lambda k: (k is None, k))
-        result.append(trials)
-
-    return result
-
-    # return [[challenge.winner_id, challenge.loser_id].sort(key=lambda k: (k is None, k))
-    #         for challenge in challenges]
+def created_challenges_list(challenges: list[Challenge]) -> list[list[Trial]]:
+    return [
+        sorted([challenge.winner, challenge.loser], key=lambda k: (k is None, k))
+        for challenge in challenges
+    ]
 
 
-def gen_random(size: int, not_in_filter: list[list[int]]) -> list[list[int]]:
+def gen_random(size: int, user: User, not_in_filter: list[list[Trial]]) -> list[list[Trial]]:
     """Returns a list with dictionaries with random trials. From that dictionary a challenge can be generated. If requested number of challenges cant be generated, the maximum possible will be returned."""
-    generated: list[dict[str, int]] = []
+    generated: list[list[Trial]] = []
     tries = 3
     while len(generated) < size and tries > 0:
-        trials = [next(random_trials), next(random_trials)]
-        trials.sort()
+        trials = sorted([next(random_trials), next(random_trials)])
         if trials[0].image.user == trials[1].image.user or \
+           trials[0].image.user == user or trials[1].image.user == user or \
            trials in not_in_filter or \
            trials in generated:
             tries -= 1
@@ -86,20 +76,20 @@ def gen_random(size: int, not_in_filter: list[list[int]]) -> list[list[int]]:
     return generated
 
 
-def gen_sametrial(size: int, completed: list[Challenge],
-                  not_in_filter: list[list[int]]) -> list[list[int]]:
+def gen_sametrial(size: int, completed: list[Challenge], user: User,
+                  not_in_filter: list[list[Trial]]) -> list[list[Trial]]:
     if random_trials == None or len(completed) == 0:
         return []
 
     same_trials = [chall.winner for chall in completed if chall.winner] \
         + [chall.loser for chall in completed if chall.loser]
 
-    generated: list[dict[str, int]] = []
+    generated: list[list[Trial]] = []
     tries = 3
     while len(generated) < size:
-        trials: list[list[int]] = [next(random_trials), choice(same_trials)]
-        trials.sort()
+        trials: list[list[Trial]] = sorted([next(random_trials), choice(same_trials)])
         if trials[0].image.user == trials[1].image.user or \
+           trials[0].image.user == user or trials[1].image.user == user or \
            trials in not_in_filter or \
            trials in generated:
             tries -= 1
@@ -108,8 +98,8 @@ def gen_sametrial(size: int, completed: list[Challenge],
     return generated
 
 
-def gen_triangle(size: int, completed: list[Challenge],
-                 not_in_filter: list[list[int]]) -> list[dict[str, int]]:
+def gen_triangle(size: int, completed: list[Challenge], user: User,
+                 not_in_filter: list[list[Trial]]) -> list[list[Trial]]:
     # creating the trials for no triangular winner checks
     # This functio will generate less triangular checks when they are not available.
     # Which is probably what I want.
@@ -117,28 +107,29 @@ def gen_triangle(size: int, completed: list[Challenge],
     losers = []
     for challenge in completed:
         if challenge.winner_id != None and challenge.loser_id != None:
-            winners.append(challenge.winner_id)
-            losers.append(challenge.loser_id)
+            winners.append(challenge.winner)
+            losers.append(challenge.loser)
 
     # TODO this can be optimised, because I need only a few
     # I can probably make some yield function.
     double_same_trials = list(set(winners).intersection(losers))
     generated = []
-    for trial_id in double_same_trials:
+    for trial in double_same_trials:
         winner, loser = None, None
         for challenge in completed:
-            if challenge.winner_id == trial_id:
+            if challenge.winner == trial:
                 loser = challenge.loser
-            if challenge.loser_id == trial_id:
+            if challenge.loser == trial:
                 winner = challenge.winner
 
-        trials = [winner, loser]
-        trials.sort()
-        if winner.image.user != loser.image.user and \
-           trials not in not_in_filter and \
-           trials not in generated:
-            generated.append(trials)
+        trials = sorted([winner, loser])
+        if trials[0].image.user == trials[1].image.user or \
+           trials[0].image.user == user or trials[1].image.user == user or \
+           trials in not_in_filter or \
+           trials in generated:
+            continue
 
+        generated.append(trials)
         if len(generated) == size:
             break
     return generated
--- tests/functionality/test_generate.py
+++ tests/functionality/test_generate.py
@@ -3,6 +3,8 @@ import pytest
 from facelo.challenge.generate import gen_random, gen_sametrial, created_challenges_list
 from facelo.challenge.views import get_recent_challenges, get_recent_challenges
 
+from facelo.trial.models import Trial
+
 
 @pytest.mark.usefixtures("db")
 class TestGenerate:
@@ -13,11 +15,13 @@ class TestGenerate:
     def test_gen_chall_data_random(self, user, question):
 
         existing_challs = created_challenges_list(get_recent_challenges(user, question, 62))
-        chall_data = gen_random(12, existing_challs)
-        assert chall_data != gen_random(12, existing_challs)
+        chall_data = gen_random(12, user, existing_challs)
+        assert chall_data != gen_random(12, user, existing_challs)
         for trial_pair in chall_data:
             assert trial_pair[0] < trial_pair[1]
             assert trial_pair not in existing_challs
+            assert trial_pair[0].image.user != user
+            assert trial_pair[1].image.user != user
 
     @pytest.mark.usefixtures("users", "images", "trials", "questions", "challenges")
     @pytest.mark.parametrize("users, images, trials, questions, challenges", [(10, 20, 20, 1, 300)],
@@ -27,11 +31,13 @@ class TestGenerate:
         completed = get_recent_challenges(user, question, 62)
 
         existing_challs = created_challenges_list(get_recent_challenges(user, question, 62))
-        chall_data = gen_sametrial(12, completed, existing_challs)
-        assert chall_data != gen_sametrial(12, completed, existing_challs)
+        chall_data = gen_sametrial(12, completed, user, existing_challs)
+        assert chall_data != gen_sametrial(12, completed, user, existing_challs)
         for trial_pair in chall_data:
             assert trial_pair[0] < trial_pair[1]
             assert trial_pair not in existing_challs
+            assert trial_pair[0].image.user != user
+            assert trial_pair[1].image.user != user
 
 
 # class TestFilters:
