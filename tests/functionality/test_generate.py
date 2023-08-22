587dc091fa70dfd768b6ed48a68e54bcd7c8499b
Harm <hveekeren.d@gmail.com>
2023-08-22 12:54:25 +0200

    chore: formatting

--- tests/functionality/test_generate.py
+++ tests/functionality/test_generate.py
@@ -1,20 +1,21 @@
 import pytest
 
-from facelo.challenge.generate import gen_random, gen_sametrial, created_challenges_list
-from facelo.challenge.views import get_recent_challenges, get_recent_challenges
-
-from facelo.trial.models import Trial
+from facelo.challenge.generate import (created_challenges_list, gen_random,
+                                       gen_sametrial)
+from facelo.challenge.views import get_recent_challenges
 
 
 @pytest.mark.usefixtures("db")
 class TestGenerate:
 
     @pytest.mark.usefixtures("users", "images", "trials", "questions", "challenges")
-    @pytest.mark.parametrize("users, images, trials, questions, challenges", [(10, 20, 20, 1, 300)],
+    @pytest.mark.parametrize("users, images, trials, questions, challenges",
+                             [(10, 20, 20, 1, 300)],
                              indirect=True)
     def test_gen_chall_data_random(self, user, question):
 
-        existing_challs = created_challenges_list(get_recent_challenges(user, question, 62))
+        existing_challs = created_challenges_list(
+            get_recent_challenges(user, question, 62))
         chall_data = gen_random(12, user, existing_challs)
         assert chall_data != gen_random(12, user, existing_challs)
         for trial_pair in chall_data:
@@ -24,13 +25,15 @@ class TestGenerate:
             assert trial_pair[1].image.user != user
 
     @pytest.mark.usefixtures("users", "images", "trials", "questions", "challenges")
-    @pytest.mark.parametrize("users, images, trials, questions, challenges", [(10, 20, 20, 1, 300)],
+    @pytest.mark.parametrize("users, images, trials, questions, challenges",
+                             [(10, 20, 20, 1, 300)],
                              indirect=True)
     def test_gen_chall_data_sametrial(self, user, question):
         # first find the completed challenges of that user
         completed = get_recent_challenges(user, question, 62)
 
-        existing_challs = created_challenges_list(get_recent_challenges(user, question, 62))
+        existing_challs = created_challenges_list(
+            get_recent_challenges(user, question, 62))
         chall_data = gen_sametrial(12, completed, user, existing_challs)
         assert chall_data != gen_sametrial(12, completed, user, existing_challs)
         for trial_pair in chall_data:
